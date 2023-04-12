0c0de975e491a91a4c791be8c3b65d1d70fbc9ac
Latifano <ikromilatifano76@gmail.com>
2023-04-12 15:11:20 +0700

    add comment di api.js

--- src/api.js
+++ src/api.js
@@ -20,6 +20,7 @@ export const getAllMovieDetail = async (id) => {
   return detail.data;
 };
 
+// Search Movie
 export const searchMovie = async (q) => {
   const search = await axios.get(
     `${baseUrl}/search/movie?query=${q}&page=1&api_key=${apiKey}`
