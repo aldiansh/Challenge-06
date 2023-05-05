293d66637901739a6588f7788c7c0ae2ad03a2a6
Latifano <ikromilatifano76@gmail.com>
2023-05-05 17:12:43 +0700

    ubah link API (Aldi , Romi)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,7 +25,7 @@ function Login() {
 
       let config = {
         method: "post",
-        url: `https://km4-challenge-5-api.up.railway.app/api/v1/auth/login`,
+        url: `${process.env.REACT_APP_API}/v1/auth/login`,
         headers: {
           "Content-Type": "application/json",
         },
